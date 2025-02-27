5cdbb299ad23cc79f6962ee4b178718d0122c410
atulashish2000 <atulashish2000@gmail.com>
2025-02-28 04:19:29 +0530

    Initial commit

---
+++ src/main/java/com/codingshuttle/project/uber/uberApp/services/RideService.java
@@ -0,0 +1,30 @@
+package com.codingshuttle.project.uber.uberApp.services;
+
+import com.codingshuttle.project.uber.uberApp.dto.RideRequestDto;
+import com.codingshuttle.project.uber.uberApp.entities.Driver;
+import com.codingshuttle.project.uber.uberApp.entities.Ride;
+import com.codingshuttle.project.uber.uberApp.entities.enums.RideStatus;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+
+public interface RideService {
+
+    Ride getRideById(Long rideId);
+    void matchWithDriver(RideRequestDto rideRequestDto);
+    Ride createNewRide(RideRequestDto rideRequestDto, Driver driver);
+    Ride updateRideStatus(Long rideId, RideStatus rideStatus);
+    Page<Ride> getAllRidesOfRider(Long riderId, PageRequest pageRequest);
+    Page<Ride> getAllRidesOfDriver(Long driverId, PageRequest pageRequest);
+}
+
+
+
+
+
+
+
+
+
+
+
+
